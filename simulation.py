ec658f14cc7b2fb0d119787bd0e781faff066b99
vateseif <vateseif@gmail.com>
2023-09-12 14:55:49 +0200

    Add save_video and process observation as Dict

--- simulation.py
+++ simulation.py
@@ -1,12 +1,17 @@
+import os
 import threading
+import numpy as np
 from time import sleep
 from typing import Optional
+from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.animation import FFMpegWriter
 
 from llm import Plan
 from robot import BaseRobot
-from core import AbstractSimulation
+from core import AbstractSimulation, BASE_DIR
 from config.config import SimulationConfig
-from mocks.mocks import nmpcMockOptions # TODO
+from mocks.mocks import nmpcMockOptions # TODO 
 
 
 class Simulation(AbstractSimulation):
@@ -17,14 +22,30 @@ class Simulation(AbstractSimulation):
     self.robot = BaseRobot()
     # count number of tasks solved from a plan 
     self.task_counter = 0
+    # init list of RGB frames if wanna save video
+    if self.cfg.save_video:
+      self.frames_list = []
+      self.video_name = f"{self.cfg.env_name}_{self.cfg.mock_plan}_{datetime.now().strftime('%d/%m/%Y_%H:%M:%S')}"
+      self.video_path = os.path.join(BASE_DIR, f"videos/{self.video_name}")
+
+  def _reinit_robot(self):
+    # x0 = [x, y, z, psi, dx, dy, dz]
+    gripper_x = self.observation['robot_0'][:3]
+    gripper_psi = np.array([self.observation['robot_0'][5]])
+    gripper_dx = self.observation['robot_0'][6:9]
+    x0 = np.concatenate((gripper_x, gripper_psi, gripper_dx))
+    self.robot.reset(x0)
 
   def reset(self):
     # reset pand env
     self.observation = self.env.reset()
     # reset controller
-    self.robot.reset(self.observation["robot_0"][:3])
+    self._reinit_robot()
     # count number of tasks solved from a plan 
     self.task_counter = 0
+    # init list of RGB frames if wanna save video
+    if self.cfg.save_video:
+      self.frames_list = []
 
   def create_plan(self, user_task:str, wait_s:Optional[int]=None): 
     self.plan = self.robot.create_plan(user_task) if self.cfg.mock_plan is None else nmpcMockOptions[self.cfg.mock_plan]
@@ -36,13 +57,40 @@ class Simulation(AbstractSimulation):
 
   def step(self):
     # update controller (i.e. set the current gripper position)
-    self.robot.set_x0(self.observation["robot_0"][:3])
+    self._reinit_robot()
     # compute action
     action = [self.robot.step()] # TODO: this is a list because the env may have multiple robots
     # apply action
     self.observation, _, done, _ = self.env.step(action)
+    # store RGB frames if wanna save video
+    if self.cfg.save_video:
+      frame = self.env.render("rgb_array")
+      self.frames_list.append(frame)
 
     return done
+
+  def close(self):
+    # close environment
+    self.env.close()
+    # init list of RGB frames if wanna save video
+    if self.cfg.save_video:
+      self._save_video()
+      
+
+  def _save_video(self):
+    # Create a figure and axis for plotting.
+    fig, ax = plt.subplots()
+    # Initialize the writer for saving the video.
+    writer = FFMpegWriter(fps=self.cfg.fps)
+    # Iterate through your list of RGB images and add them to the video.
+    with writer.saving(fig, self.video_path, dpi=200):
+      for i, rgb_image in enumerate(self.frames_list):
+        ax.clear()  # Clear the previous frame.
+        ax.imshow(rgb_image)  # Display the current RGB image.
+        plt.axis("off")  # Turn off axis labels and ticks.
+        # You can optionally add a title or annotation to each frame.
+        ax.set_title(f"Frame {i + 1}")
+        writer.grab_frame()  # Save the current frame to the video.
 
   def _solve_task(self, plan:str):
     self.robot.next_plan(plan, self.observation)
