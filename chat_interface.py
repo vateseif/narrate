c9290554adabc5ca4bbc1ec6b1833ac77c67691a
vateseif <vateseif@gmail.com>
2023-09-13 16:39:26 +0200

    Add simulate_stream

--- chat_interface.py
+++ chat_interface.py
@@ -1,5 +1,7 @@
 import os
+import tiktoken
 import streamlit as st
+from time import sleep
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, AIMessage
 from langchain.callbacks.base import BaseCallbackHandler
@@ -17,12 +19,24 @@ class StreamHandler(BaseCallbackHandler):
 
   def on_llm_end(self, response, **kwargs):
     self.container.markdown(self.text)
-    st.session_state.messages.append(AIMessage(content=self.text))
 
+def simulate_stream(text:str):
+  """ Function used to simulate stream in case of harcoded responses """
+  placeholder = st.empty()
+  # Simulate stream of response with milliseconds delay
+  partial_text = ""
+  for chunk in enc.decode_batch([[x] for x in enc.encode(text)]):
+      partial_text += chunk
+      sleep(0.05)
+      # Add a blinking cursor to simulate typing
+      placeholder.markdown(partial_text + "▌")
+  placeholder.markdown(text)
+  # return AI message
+  return AIMessage(content=text)
 
-stream_handler = StreamHandler()
+enc = tiktoken.encoding_for_model("gpt-4")
 os.environ["OPENAI_API_KEY"] = open('keys/gpt4.key', 'r').readline().rstrip()
-chat = ChatOpenAI(streaming=True, callbacks=[stream_handler], temperature=0)
+chat = ChatOpenAI(streaming=True, callbacks=[StreamHandler()], temperature=0)
 
 st.title("Language to Optimization")
 
@@ -46,7 +60,8 @@ if prompt := st.chat_input("What is up?"):
 
     # Display assistant response in chat message container
     with st.chat_message("ai"):
-      response = chat(st.session_state.messages)
-
+      #response = chat(st.session_state.messages)
+      ai_message = simulate_stream("")
+      st.session_state.messages.append(ai_message)
 
 
