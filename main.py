6c370b7f4f6421c970b5825ec386401009650b8d
vateseif <vateseif@gmail.com>
2023-09-14 21:46:13 +0200

    minor

--- main.py
+++ main.py
@@ -1,51 +1,57 @@
+import streamlit as st
+from langchain.schema import HumanMessage
+
 from simulation import Simulation
 
-from time import sleep
-from llm import Optimization
 
-if __name__ == "__main__":
+# Init streamlit page
+st.title("Language to Optimization")
 
-  # Run simulator
-  sim = Simulation()
-  sim.run()
+# Create sidebar
+st.sidebar.title("Choose model")
 
-  #sim.create_plan("Stack all cubes on top of cube_2.")
-  #sim.next_task()
+# Add a sidebar radio button to select the message type
+model = st.sidebar.radio("Select the model to talk to", ["Task Planner", "Optimization Designer"])
+
+# init robot simulation
+if "sim" not in st.session_state:
+  st.session_state.sim = Simulation()
+  st.session_state.sim.run()
+  st.session_state.PLAN_EXECUTABLE = False # indicates if there is a plan the OD has to execute
+  st.session_state.EXECUTE_PLAN = False # indicates if the OD has to execute plan
+
+# Initialize chat history
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+  with st.chat_message(message.type):
+    st.markdown(message.content)
+
+# Accept user input
+if prompt := st.chat_input("What should the robot do?"):
+  # Add user message to chat history
+  st.session_state.messages.append(HumanMessage(content=prompt))
+  # Display user message in chat message container
+  with st.chat_message("human"):
+    st.markdown(prompt)
+
+  # Display assistant response in chat message container
+  with st.chat_message("ai"):
+    if model == "Task Planner":
+      st.session_state.sim.create_plan(prompt, solve=False) 
+      st.session_state.PLAN_EXECUTABLE = True
+    elif model == "Optimization Designer":
+      st.session_state.sim._solve_task(prompt)
+
+if st.session_state.EXECUTE_PLAN:
+  st.session_state.sim.execute_plan()
+  st.session_state.EXECUTE_PLAN = False
+
+if st.session_state.PLAN_EXECUTABLE and st.button("Execute plan"):
+  st.session_state.EXECUTE_PLAN = True
+  st.session_state.PLAN_EXECUTABLE = False
+  st.experimental_rerun()
   
-  """
-  sleep(1)
 
-  
-  optimization = Optimization(
-    objective = "ca.norm_2(x - sponge)**2",
-    constraints = [
-      "0.04685 - ca.norm_2(x - sponge)",
-      "0.04685 - ca.norm_2(x - plate)"
-    ]  
-  )
-  sim.robot.MPC.apply_gpt_message(optimization, sim.observation)
-
-
-  sleep(7)
-  
-  sim.robot.close_gripper()
-
-  sleep(2)
-  optimization = Optimization(
-    objective = "ca.norm_2(x - plate - np.array([0., 0., 0.02]))**2",
-    constraints = [
-      "0.045 - ca.norm_2(x - plate + np.array([0., 0.04, 0.0]))",
-      "0.045 - ca.norm_2(x - plate - np.array([0., 0.04, 0.0]))",
-    ]  
-  )
-  sim.robot.MPC.apply_gpt_message(optimization, sim.observation)
-
-  sleep(5)
-
-  optimization = Optimization(
-    objective = "ca.norm_2(x - plate - np.array([0.05*ca.cos(2*t), 0.05*ca.sin(2*t), 0.02]))**2",
-    constraints = []  
-  )
-  sim.robot.MPC.apply_gpt_message(optimization, sim.observation)
-  """
-  
