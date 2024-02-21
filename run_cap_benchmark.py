85c3789bad1b45eb25615844869a856fd970339c
Antonio Arbues <avarbues@gmail.com>
2024-02-21 18:19:05 +0100

    changed tasks prompts

--- run_cap_benchmark.py
+++ run_cap_benchmark.py
@@ -1,9 +1,15 @@
 import requests
 from tqdm import tqdm
+from random import sample
 base_url = 'http://localhost:8080/'
 
-N_EXPERIMENTS = 50
-prompt = "stack all the cubes in a single pile"
+colors = ["red", "green", "blue", "orange"]
+prompts = ["make a stack of cubes on top of the {} one".format(*sample(colors, 1)),
+           "rearrange cubes to write the letter L on the table. keep {} at its location".format(*sample(colors, 1)),
+           "build a pyramid with the {} and {} cubes at the base and {} cube at the top. keep {} cube at its original position.".format(*(2*sample(colors, 3)))]
+
+N_EXPERIMENTS = 1
+prompt = prompts[0]
 
 for i in tqdm(range(N_EXPERIMENTS)):
     response = requests.post(base_url+'cap', json={"content": prompt})
