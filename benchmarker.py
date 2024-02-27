af6f8cb4573b7043c468c0a968d0488ece698be5
Seif (MAcbook M3) <vateseif@gmail.com>
2024-02-27 11:31:35 +0100

    visualize plot of collaborative robots

--- benchmarker.py
+++ benchmarker.py
@@ -1,8 +1,9 @@
 import json
 import numpy as np
 import streamlit as st
-from sqlalchemy import func
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -13,21 +14,29 @@ import os
 wd = os.getcwd()
 
 
-method = st.selectbox('Select a method', options=["ours", "ours_objective", "cap_stack", "cap_pyramid", "cap_L"])
+method = st.selectbox('Select a method', options=["ours", "ours_objective", "cap"])
+task = st.selectbox('Select a task', options=["stack", "pyramid", "L", "CleanPlate", "Sponge"])
 
 
-if "ours" in method:
-	db_name = f'data/{method}/DBs/cubes.db'
-elif 'cap' in method:
-	if 'stack' in method:
-		db_name = f'data/cap/DBs/stacking.db'
-	elif 'pyramid' in method:
-		db_name = f'data/cap/DBs/pyramid.db'
-	elif 'L' in method:
-		db_name = f'data/cap/DBs/letter_l.db'
+if method == "ours":
+	if task in ["stack", "pyramid", "L"]:
+		db_name = f'data/{method}/DBs/cubes.db'
+	else:
+		db_name = f'data/{method}/DBs/{task}.db'	
+elif method == "ours_objective":
+	if task == "CleanPlate":
+		db_name = f'data/{method}/DBs/CleanPlate.db'
+	else:
+		db_name = f'data/{method}/DBs/cubes.db'
+elif method == "cap":
+	if task == "stack":
+		db_name = f'data/{method}/DBs/stacking.db'
+	elif task == 'pyramid':
+		db_name = f'data/{method}/DBs/pyramid.db'
+	elif task == 'L':
+		db_name = f'data/{method}/DBs/letter_l.db'
 	else:
 		raise ValueError("Invalid method name")
-	method = "cap"
 
 engine = create_engine(f'sqlite:///{db_name}')
 Base.metadata.create_all(engine)
@@ -49,19 +58,39 @@ def plot_trajectory(episode_id):
 		except:
 			st.write("No state trajectory available for this episode.")
 			return
-		state_trajectory = [np.array(s) for s in state_trajectory]
+		if task in ["stack", "pyramid", "L", "CleanPlate"]:
+			state_trajectory = [np.array(s) for s in state_trajectory]
 
-		efficiency = sum([np.linalg.norm(state_trajectory[i+1][:3] - state_trajectory[i][:3]) for i in range(len(state_trajectory)-1)])
-		# 3d plot of the trajectory
-		fig = plt.figure()
-		ax = fig.add_subplot(111, projection='3d')
-		ax.set_title("3D trajectory. Cumulative travel = {:.2f}m".format(efficiency))
-		ax.plot([s[0] for s in state_trajectory], [-s[1] for s in state_trajectory], [s[2] for s in state_trajectory])
+			efficiency = sum([np.linalg.norm(state_trajectory[i+1][:3] - state_trajectory[i][:3]) for i in range(len(state_trajectory)-1)])
+			# 3d plot of the trajectory
+			fig = plt.figure()
+			ax = fig.add_subplot(111, projection='3d')
+			ax.set_title("3D trajectory. Cumulative travel = {:.2f}m".format(efficiency))
+			ax.plot([s[0] for s in state_trajectory], [-s[1] for s in state_trajectory], [s[2] for s in state_trajectory])
 
-		# plot start and end
-		ax.scatter(state_trajectory[0][0], -state_trajectory[0][1], state_trajectory[0][2], c='r', marker='o')
-		ax.scatter(state_trajectory[-1][0], -state_trajectory[-1][1], state_trajectory[-1][2], c='g', marker='o')
-		st.pyplot(fig)
+			# plot start and end
+			ax.scatter(state_trajectory[0][0], -state_trajectory[0][1], state_trajectory[0][2], c='r', marker='o')
+			ax.scatter(state_trajectory[-1][0], -state_trajectory[-1][1], state_trajectory[-1][2], c='g', marker='o')
+			st.pyplot(fig)
+		else:
+			state_trajectory_left = [np.array(s) for i,s in enumerate(state_trajectory) if i%2==0]
+			state_trajectory_right = [np.array(s) for i,s in enumerate(state_trajectory) if i%2==1]
+
+			efficiency_left = sum([np.linalg.norm(state_trajectory_left[i+1][:3] - state_trajectory_left[i][:3]) for i in range(len(state_trajectory_left)-1)])
+			efficiency_right = sum([np.linalg.norm(state_trajectory_right[i+1][:3] - state_trajectory_right[i][:3]) for i in range(len(state_trajectory_right)-1)])
+
+			fig = plt.figure()
+			ax = fig.add_subplot(111, projection='3d')
+			ax.set_title("3D trajectory. Cumulative travel = {:.2f}m + {:.2f}m".format(efficiency_left, efficiency_right))
+			ax.plot([s[0] for s in state_trajectory_left], [-s[1] for s in state_trajectory_left], [s[2] for s in state_trajectory_left], label="left robot")
+			ax.plot([s[0] for s in state_trajectory_right], [-s[1] for s in state_trajectory_right], [s[2] for s in state_trajectory_right], label="right robot")
+
+			# plot start and end
+			ax.scatter(state_trajectory_left[0][0], -state_trajectory_left[0][1], state_trajectory_left[0][2], c='r', marker='o')
+			ax.scatter(state_trajectory_left[-1][0], -state_trajectory_left[-1][1], state_trajectory_left[-1][2], c='g', marker='o')
+			ax.scatter(state_trajectory_right[0][0], -state_trajectory_right[0][1], state_trajectory_right[0][2], c='r', marker='o')
+			ax.scatter(state_trajectory_right[-1][0], -state_trajectory_right[-1][1], state_trajectory_right[-1][2], c='g', marker='o')
+			st.pyplot(fig)
 	except Exception as e:
 		return
 
@@ -82,22 +111,19 @@ def plot_mpc_times(episode_id):
 
 
 def get_task_type(episode_id):
-	header = "Task type: "
+	if task in ["CleanPlate", "Sponge"]:
+			return task
+	
 	if "ours" in method:
 		instruction = session.query(Epoch).filter_by(episode_id=episode_id).order_by(Epoch.time_step.asc()).first().content
 		if "stack" in instruction:
-			return header+"stack"
+			return "stack"
 		elif "L" in instruction:
-			return header+"L"
+			return "L"
 		else:
-			return header+"pyramid"
+			return "pyramid"
 	else:
-		if "stack" in method:
-			return header+"stack"
-		elif "letter" in method:
-			return header+"L"
-		else:
-			return header+"pyramid"
+		return task
 
 def append_message(message:dict):
 	# Function to append a message to the chat history
@@ -119,13 +145,15 @@ def append_message(message:dict):
 
 	
 # count number of episodes in db
-n_episodes = session.query(func.count(Episode.id)).scalar()  # Assuming 'id' is a primary key column
-episodes = [i for i in range(1, n_episodes+1)]
-episode_id = st.number_input('Enter an Episode ID', min_value=1, max_value=n_episodes, value=1)
+episodes = session.query(Episode).all()  # Assuming 'id' is a primary key column
+episodes = [e for e in episodes if get_task_type(e.id) == task]
+episode_id = st.number_input('Enter an Episode ID', min_value=1, max_value=len(episodes)-1, value=1)
 
-epochs = session.query(Epoch).filter_by(episode_id=episode_id).order_by(Epoch.time_step.desc()).all()
+epochs = session.query(Epoch).filter_by(episode_id=episodes[episode_id].id).order_by(Epoch.time_step.desc()).all()
 st.session_state.messages = []
 for e in epochs:
+	if get_task_type(e.episode_id) != task:
+		continue
 	if e.image != "":
 		if "ours" in method:
 			image_url = e.image.replace('data', f'data/{method}')
@@ -136,26 +164,26 @@ for e in epochs:
 		st.session_state.messages += [{"type":e.role, 'content':e.content.replace("#","\n")}]
 
 
-# h3 with task type
-st.header(get_task_type(episode_id))
+if not st.session_state.messages == []:
+	# read user query
+	append_message(st.session_state.messages[-1])
+	# Visualize last image to see if task completed succesfully
+	append_message(st.session_state.messages[0])
 
-# read user query
-append_message(st.session_state.messages[-1])
-# Visualize last image to see if task completed succesfully
-append_message(st.session_state.messages[0])
+	# plot 3D trajectory
+	plot_trajectory(episode_id)
 
-# plot 3D trajectory
-plot_trajectory(episode_id)
+	# plot MPC solve times
+	plot_mpc_times(episode_id)
 
-# plot MPC solve times
-plot_mpc_times(episode_id)
+	# Display chat messages from history on app rerun
+	for message in st.session_state.messages:
+		append_message(message)
 
 # close db session
 session.close()
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-	append_message(message)
+
 
 	
 
